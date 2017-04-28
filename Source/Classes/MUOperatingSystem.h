71cc75978fdfe64b2bbaa37a6aecda376833dfa3
wendelpercia <wendel.percia@gmail.com>
2017-04-28 17:59:51 -0300

    Teste

--- Source/Classes/MUOperatingSystem.h
+++ Source/Classes/MUOperatingSystem.h
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-
 typedef NS_ENUM(NSInteger, MUOperatingSystemVersion) {
     MUMBLE_OS_UNKNOWN,
     
