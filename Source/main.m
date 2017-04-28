3bbc37b1590ac5af7c52676a60c83bcf516894e9
wendelpercia <wendel.percia@gmail.com>
2017-04-28 17:50:22 -0300

    Initial Commit

---
+++ Source/main.m
@@ -0,0 +1,10 @@
+// Copyright 2009-2010 The 'Mumble for iOS' Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, @"UIApplication", @"MUApplicationDelegate");
+    [pool release];
+    return retVal;
+}
